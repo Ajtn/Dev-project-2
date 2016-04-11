ad4258ed327f0efcb748f2e39e9fec4cb64b2004
asdcrisafi <marcus.crisafi@gmail.com>
2016-04-10 23:33:22 -0700

    Revert "Drafting database interaction"

--- InventoryManager/src/GameMain.cs
+++ InventoryManager/src/GameMain.cs
@@ -7,31 +7,23 @@ namespace MyGame
     {
         public static void Main()
         {
-            DatabaseManager inventoryDB = new DatabaseManager("sql6.freemysqlhosting.net", "sql6114576",
-                                                               "sql6114576", "BzSIN9yCih");
-            if (inventoryDB.OpenDBConnection())
+            //Open the game window
+            SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
+            SwinGame.ShowSwinGameSplashScreen();
+            
+            //Run the game loop
+            while(false == SwinGame.WindowCloseRequested())
             {
-                //Open the game window
-                SwinGame.OpenGraphicsWindow("GameMain", 800, 600);
-                SwinGame.ShowSwinGameSplashScreen();
-
-                //Run the game loop
-                while (false == SwinGame.WindowCloseRequested())
-                {
-                    //Fetch the next batch of UI interaction
-                    SwinGame.ProcessEvents();
-
-                    //Clear the screen and draw the framerate
-                    SwinGame.ClearScreen(Color.White);
-                    SwinGame.DrawFramerate(0, 0);
-
-                    //Draw onto the screen
-                    SwinGame.RefreshScreen(60);
-                }
+                //Fetch the next batch of UI interaction
+                SwinGame.ProcessEvents();
+                
+                //Clear the screen and draw the framerate
+                SwinGame.ClearScreen(Color.White);
+                SwinGame.DrawFramerate(0,0);
+                
+                //Draw onto the screen
+                SwinGame.RefreshScreen(60);
             }
-            else { Console.WriteLine("Error Opening DB Connection"); }
-
-
         }
     }
 }
