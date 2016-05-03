8a3d5796fa5e01d76cc6476abed2c68e0c21abf5
funki-town <4964276@student.swin.edu.au>
2016-05-03 19:07:51 +1000

    add login screen

---
+++ InventoryManager/src/UIElements/Login.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+    class Login
+    {
+        public bool _authenticated;
+        public string _username;
+        public string _password;
+        public const string USERNAME = "admin";
+        public const string PASSWORD = "admin";
+
+        public Login()
+        {
+            _authenticated = false;
+        }
+
+        public bool Authenticated
+        {
+            get
+            {
+                return _authenticated;
+            }
+
+
+        }
+
+        public void Try(string st1, string st2)
+        {
+            _username = st1;
+            _password = st2;
+            if (_username == USERNAME && _password == PASSWORD)
+            {
+                _authenticated = true;
+
+            }
+
+
+        }
+    }
+}
+
