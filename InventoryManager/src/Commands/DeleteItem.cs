5aad31410d8ef245c32f681429cd9514e2ce232f
Lionel1232 <lionel1232@gmail.com>
2016-05-09 23:22:57 +1000

    Incomplete delete functionality

---
+++ InventoryManager/src/Commands/DeleteItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyGame
+{
+	class DeleteItem : Command
+	{
+		public override void Do(int x, int y)
+		{
+			string[] row;
+
+			row = GameMain.pTable.GetRow (x, y);
+
+			if (GameMain.pTable.pTableName == )
+		}
+	}
+}
